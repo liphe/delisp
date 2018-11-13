9f3bada0f1d258bab25f754f1d1e573fd67856e0
David Vázquez Púa <davazp@gmail.com>
2018-11-14 00:44:31 +0100

    Improve reader
    - Support for tracking the location of expressions Generation of
    - user-friendly errors

--- packages/delisp-core/__tests__/reader.ts
+++ packages/delisp-core/__tests__/reader.ts
@@ -1,47 +1,37 @@
-import { readFromString } from "../src/";
+import { readFromString } from "../src/reader";
 
 describe("Reader", () => {
-  // it("should read basic booleans", () => {
-  //   expect(readFromString("true")).toEqual({
-  //     type: "LiteralBoolean",
-  //     value: true
-  //   });
+  it("should read basic booleans", () => {
+    expect(readFromString("(1 2 3)")).toEqual({
+      type: "LiteralBoolean",
+      value: true
+    });
+  });
 
-  //   expect(readFromString("false")).toEqual({
-  //     type: "LiteralBoolean",
-  //     value: false
+  // it.skip("should read basic expessions", () => {
+  //   expect(readFromString("1")).toEqual({ type: "LiteralNumber", value: 1 });
+  //   expect(readFromString("x")).toEqual({ type: "LiteralSymbol", value: "x" });
+  //   expect(readFromString("()")).toEqual({ type: "List", value: [] });
+  //   expect(readFromString("(1 2)")).toEqual({
+  //     type: "List",
+  //     value: [
+  //       { type: "LiteralNumber", value: 1 },
+  //       { type: "LiteralNumber", value: 2 }
+  //     ]
+  //   });
+  //   // expect(readFromString("(whatever (stuff)"))
+  // });
+  // it.skip("should parse nested lists", () => {
+  //   expect(readFromString(" ( 1 ( 2 ) 3 )")).toEqual({
+  //     type: "List",
+  //     value: [
+  //       { type: "LiteralNumber", value: 1 },
+  //       {
+  //         type: "List",
+  //         value: [{ type: "LiteralNumber", value: 2 }]
+  //       },
+  //       { type: "LiteralNumber", value: 3 }
+  //     ]
   //   });
   // });
-
-  it("should read basic expessions", () => {
-    expect(readFromString("1")).toEqual({ type: "LiteralNumber", value: 1 });
-
-    expect(readFromString("x")).toEqual({ type: "LiteralSymbol", value: "x" });
-
-    expect(readFromString("()")).toEqual({ type: "List", value: [] });
-
-    expect(readFromString("(1 2)")).toEqual({
-      type: "List",
-      value: [
-        { type: "LiteralNumber", value: 1 },
-        { type: "LiteralNumber", value: 2 }
-      ]
-    });
-
-    // expect(readFromString("(whatever (stuff)"))
-  });
-
-  it("should parse nested lists", () => {
-    expect(readFromString(" ( 1 ( 2 ) 3 )")).toEqual({
-      type: "List",
-      value: [
-        { type: "LiteralNumber", value: 1 },
-        {
-          type: "List",
-          value: [{ type: "LiteralNumber", value: 2 }]
-        },
-        { type: "LiteralNumber", value: 3 }
-      ]
-    });
-  });
 });
--- packages/delisp-core/src/reader.ts
+++ packages/delisp-core/src/reader.ts
@@ -1,103 +1,247 @@
-import { List, Syntax } from "./syntax";
+//
+// reader.ts --- The reader for Delisp
+//
+
+// import { List, Syntax } from "./syntax";
+
+export interface Location {
+  offset: number;
+}
 
 class Input {
-  input: string;
-  offset: number;
+  private input: string;
+  private offset: number;
 
   constructor(input: string, offset: number = 0) {
     this.input = input;
     this.offset = offset;
   }
 
-  readChar(): [string, Input] {
-    return [this.input[this.offset], new Input(this.input, this.offset + 1)];
+  readChar(): [string | null, Input] {
+    if (this.offset >= this.input.length) {
+      throw new Error(`End of file`);
+    } else {
+      return [this.input[this.offset], new Input(this.input, this.offset + 1)];
+    }
   }
 
-  consume(n: number) {
-    return new Input(this.input, this.offset + n);
+  readChars(n: number): [string, Input] {
+    return [
+      this.input.slice(this.offset, this.offset + n),
+      new Input(this.input, this.offset + n)
+    ];
   }
 
   toString() {
     return this.input.slice(this.offset);
   }
+
+  location(): Location {
+    return { offset: this.offset };
+  }
 }
 
-type ParserResult = [Syntax, Input];
+// `Parser` implements a parsers with a monadic interface.
+//
+// A parser will produce a `ParserResult` of of a given type, or it
+// will fail. Failed parsers should return information about what they
+// expected to parse to facilitate generating friendly error messages.
+//
 
-class Parser {
-  run: (input: Input) => ParserResult;
+type ParserResult<A> = ParserSuccess<A> | ParserError;
 
-  constructor(fn: (input: Input) => ParserResult) {
+interface ParserSuccess<A> {
+  status: "success";
+  value: A;
+  moreInput: Input;
+}
+
+interface ParserError {
+  status: "error";
+  expected?: string;
+  reasons: ParserError[];
+}
+
+class Parser<A> {
+  run: (input: Input) => ParserResult<A>;
+
+  constructor(fn: (input: Input) => ParserResult<A>) {
     this.run = fn;
   }
 
-  parse(input: string) {
-    return this.run(new Input(input, 0));
+  static of<A>(value: A) {
+    return new Parser(moreInput => {
+      const result: ParserSuccess<A> = { status: "success", value, moreInput };
+      return result;
+    });
+  }
+
+  description(desc: string) {
+    return new Parser(input => {
+      const result = this.run(input);
+      if (result.status === "success") {
+        return result;
+      } else {
+        return {
+          status: "error",
+          expected: desc,
+          reasons: [result]
+        };
+      }
+    });
+  }
+
+  chain<B>(fn: (value: A) => Parser<B>) {
+    return new Parser<B>((input: Input) => {
+      const result = this.run(input);
+      if (result.status !== "success") {
+        return result;
+      }
+      const { value, moreInput } = result;
+      return fn(value).run(moreInput);
+    });
+  }
+
+  then<B>(p: Parser<B>): Parser<B> {
+    return this.chain(_ => p);
+  }
+
+  skip(p: Parser<unknown>): Parser<A> {
+    return this.chain(x => p.map(_ignored => x));
+  }
+
+  or(makeAlternative: () => Parser<A>) {
+    return new Parser((input: Input) => {
+      // Try the current parser
+      const result = this.run(input);
+      if (result.status === "success") {
+        return result;
+      }
+      // Try the alternative parser
+      const alternativeResult = makeAlternative().run(input);
+      if (alternativeResult.status === "success") {
+        return alternativeResult;
+      }
+      // If both failed, keep track of expected values so we can come
+      // back to them later!
+      return {
+        status: "error",
+        reasons: [...result.reasons, ...alternativeResult.reasons]
+      };
+    });
+  }
+
+  map<B>(f: (x: A) => B): Parser<B> {
+    return this.chain(a => Parser.of(f(a)));
   }
 }
 
-const Atom = new Parser(input => {
-  const matches = input.toString().match(/^([^)\s]*)/);
-  if (matches === null) {
-    throw new Error("Unexpected");
-  }
-  const token = matches[1];
-  const rest = input.consume(token.length);
+function alternatives<A>(...alternatives: Parser<A>[]) {
+  return alternatives.reduce((p1, p2) => p1.or(() => p2));
+}
 
-  if (token.match(/[0-9]+/)) {
-    const value = parseInt(token, 10);
-    return [{ type: "LiteralNumber", value }, rest];
-  } else {
-    return [{ type: "LiteralSymbol", value: token }, rest];
-  }
-});
+function delimited<A>(
+  left: Parser<unknown>,
+  p: Parser<A>,
+  right: Parser<unknown>
+) {
+  return left.then(p).skip(right);
+}
 
-const ListExpr = new Parser(
-  (input): [List, Input] => {
-    // const [,input] = skipSpaces.run(input_);
+function many<A>(parser: Parser<A>): Parser<A[]> {
+  return parser
+    .chain(first => {
+      return many(parser).map(rest => [first, ...rest]);
+    })
+    .or(() => Parser.of([]));
+}
 
-    const [char, rest] = input.readChar();
-    if (char === ")") {
-      return [{ type: "List", value: [] }, rest];
+// The reader is organized in 3 layers of parsing:
+//
+// - Lexical level
+// - S-Expression level
+// - Syntactic level
+//
+
+//
+// Lexical parsing
+//
+
+function flags(re: RegExp) {
+  var s = "" + re;
+  return s.slice(s.lastIndexOf("/") + 1);
+}
+
+function anchoredRegexp(re: RegExp) {
+  return RegExp("^(?:" + re.source + ")", flags(re));
+}
+
+const regex = (regex: RegExp) => {
+  const anchored = anchoredRegexp(regex);
+  return new Parser<string>(input => {
+    const match = anchored.exec(input.toString());
+
+    if (match === null) {
+      return {
+        status: "error",
+        expected: `match for ${regex}`,
+        reasons: []
+      };
     }
 
-    const [atom, rest2] = Expr.run(input);
-    const [restA, rest3] = ListExpr.run(rest2);
+    const value = match[0];
+    const [, moreInput] = input.readChars(value.length);
+    return {
+      status: "success",
+      value,
+      moreInput
+    };
+  });
+};
 
-    return [
-      {
-        type: "List",
-        value: [atom, ...(restA as List).value]
-      },
-      rest3
-    ];
-  }
+//
+// S-Expression parser
+//
+
+type SExpr = number | string | SExprList;
+interface SExprList extends Array<SExpr> {}
+
+const spaces = regex(/^\s*/);
+
+function spaced<A>(x: Parser<A>) {
+  return delimited(spaces, x, spaces);
+}
+
+const number: Parser<SExpr> = regex(/[0-9]+/)
+  .map(x => parseInt(x, 10))
+  .description("number");
+
+// const doubleQuote = regex(/"/).description("double quote");
+//
+// const stringChar = regex(/[^"\\]/).description("non-escaped string character");
+// const stringEscapedChar = regex(/\\[n\\]/).description(
+//   "escaped string character"
+// );
+
+const symbol: Parser<SExpr> = regex(/[a-zA-Z+<>!@$%^*/-]+/).description(
+  "symbol"
 );
 
-const skipSpaces = (parser: Parser) =>
-  new Parser(
-    (input: Input): ParserResult => {
-      const [char, rest] = input.readChar();
-      if (char === " ") {
-        return skipSpaces(parser).run(rest);
-      } else {
-        return parser.run(input);
-      }
-    }
-  );
+const atom: Parser<SExpr> = alternatives(number, symbol).description("atom");
 
-const Expr = skipSpaces(
-  new Parser(input => {
-    const [first, rest] = input.readChar();
-    if (first === "(") {
-      return ListExpr.run(rest);
-    } else {
-      return Atom.run(input);
-    }
-  })
-);
+const leftParen = regex(/\(/).description("open parenthesis");
+const rightParen = regex(/\)/).description("close parenthesis");
 
-export function readFromString(input: string): Syntax {
-  const [syntax] = Expr.parse(input);
-  return syntax;
+function list<A>(x: Parser<A>) {
+  return leftParen.then(many(x)).skip(rightParen);
+}
+
+const sexpr: Parser<SExpr> = spaced(atom.or(() => list(sexpr)));
+
+//
+// Parser a Delisp expression from a string
+//
+export function readFromString(str: string) {
+  const input = new Input(str);
+  return sexpr.run(input);
 }
