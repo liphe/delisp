ca6f5398460a9472688fcff9a47b6dd7272fe993
Tijn Kersjes <tijn@divbyzero.nl>
2018-11-21 19:03:52 +0100

    Implement user definitions

--- packages/delisp-core/src/compiler.ts
+++ packages/delisp-core/src/compiler.ts
@@ -5,6 +5,44 @@ const debug = require('debug')('delisp:compiler')
 
 type JSAST = object;
 
+function compileList(fn: ASExpr, args: ASExpr[]): JSAST {
+  if (fn.type === 'symbol') {
+    switch(fn.name) {
+      case 'define':
+        if (args.length !== 2)
+          throw new Error('Define needs exactly 2 arguments')
+
+        const variable = args[0]
+        if (variable.type !== 'symbol')
+          throw new Error('First argument of define should be a symbol');
+
+        return {
+          type: 'AssignmentExpression',
+          operator: '=',
+          left: {
+            type: 'MemberExpression',
+            computed: true,
+            object: {
+              type: 'Identifier',
+              name: 'env'
+            },
+            property: {
+              type: 'Literal',
+              value: variable.name
+            }
+          },
+          right: compile(args[1])
+        }
+    }
+  }
+
+  return {
+    type: 'CallExpression',
+    callee: compile(fn),
+    arguments: args.map(compile)
+  };
+}
+
 export function compile(syntax: ASExpr): JSAST {
   switch(syntax.type) {
     case 'number':
@@ -15,6 +53,7 @@ export function compile(syntax: ASExpr): JSAST {
     case 'symbol':
       return {
         type: 'MemberExpression',
+        computed: true,
         object: {
           type: 'Identifier',
           name: 'env'
@@ -22,8 +61,7 @@ export function compile(syntax: ASExpr): JSAST {
         property: {
           type: 'Literal',
           value: syntax.name
-        },
-        computed: true
+        }
       };
     case 'string':
       return {
@@ -32,11 +70,7 @@ export function compile(syntax: ASExpr): JSAST {
       };
     case 'list':
       const [fn, ...args] = syntax.elements;
-      return {
-        type: 'CallExpression',
-        callee: compile(fn),
-        arguments: args.map(compile)
-      };
+      return compileList(fn, args)
   }
 }
 
