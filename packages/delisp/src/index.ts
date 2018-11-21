61f2dbca443a2f4fcf68e4e9cbd5742d3aa8d660
Tijn Kersjes <tijn@divbyzero.nl>
2018-11-21 17:41:18 +0100

    Use repl module for repl

--- packages/delisp/src/index.ts
+++ packages/delisp/src/index.ts
@@ -1,25 +1,19 @@
-import readline from "readline";
+import repl from "repl";
 
 import { readFromString } from "@delisp/core";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: "λ "
-});
+const delispEval = (
+  cmd: string,
+  _context: object,
+  _filename: string,
+  callback: Function
+) => {
+  callback(null, readFromString(cmd));
+};
 
-rl.prompt();
+const replServer = repl.start({ prompt: "λ ", eval: delispEval });
 
-rl.on("line", line => {
-  try {
-    const syntax = readFromString(line);
-    console.log(syntax);
-  } catch (err) {
-    console.error(err);
-  } finally {
-    rl.prompt();
-  }
-}).on("close", () => {
+replServer.on("exit", () => {
   console.log("\n; bye!");
   process.exit(0);
 });
