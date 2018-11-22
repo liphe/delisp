6d21844dbbf0fdcd6b6ef20d96b68faef5d7daf7
David Vázquez Púa <davazp@gmail.com>
2018-11-22 14:50:06 +0100

    Add some evaluation tests

---
+++ packages/delisp-core/__tests__/eval.ts
@@ -0,0 +1,37 @@
+import { readFromString } from "../src/reader";
+import { evaluate } from "../src/eval";
+
+function evaluateString(str: string): any {
+  return evaluate(readFromString(str));
+}
+
+describe("Evaluation", () => {
+  describe("Numbers", () => {
+    it("should self-evaluate", () => {
+      expect(evaluateString("0")).toBe(0);
+      expect(evaluateString("1")).toBe(1);
+      expect(evaluateString("-1")).toBe(-1);
+    });
+  });
+
+  describe("Strings", () => {
+    it("should self-evaluate", () => {
+      expect(evaluateString('""')).toBe("");
+      expect(evaluateString('"foo"')).toBe("foo");
+      expect(evaluateString('"a\\nb"')).toBe("a\nb");
+    });
+  });
+
+  describe("Function calls", () => {
+    it("should evaluate to the right value", () => {
+      expect(evaluateString("(+ 1 2)")).toBe(3);
+      expect(evaluateString("(+ (+ 1 1) 2)")).toBe(4);
+    });
+  });
+
+  describe("Lambda abstractions", () => {
+    it("should be able to be called", () => {
+      expect(evaluateString("((lambda (x y) y) 4 5)")).toBe(5);
+    });
+  });
+});
