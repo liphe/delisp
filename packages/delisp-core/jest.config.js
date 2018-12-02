7bb7b6a6987f9b97027568253f09aae864b2ed6b
David Vázquez Púa <davazp@gmail.com>
2018-12-02 11:19:59 +0100

    Configure jest coverage for delisp-core

--- packages/delisp-core/jest.config.js
+++ packages/delisp-core/jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverageFrom: ["src/**/*.{ts}"]
 };
