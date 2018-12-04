4295987eeb6e3efb9a918b11621ee1c8b8822369
David Vázquez Púa <davazp@gmail.com>
2018-12-04 21:09:58 +0100

    Implement pretty printing

--- packages/delisp-core/src/prettier.ts
+++ packages/delisp-core/src/prettier.ts
@@ -5,6 +5,8 @@
 // https://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf
 //
 
+const INDENT_SPACES = 2;
+
 interface DocNil {
   type: "nil";
 }
@@ -21,7 +23,13 @@ interface DocLine {
   doc: Doc;
 }
 
-export type Doc = DocNil | DocText | DocLine;
+interface DocUnion {
+  type: "union";
+  x: Doc;
+  y: Doc;
+}
+
+export type Doc = DocNil | DocText | DocLine | DocUnion;
 
 export const nil: Doc = { type: "nil" };
 
@@ -41,7 +49,7 @@ export const line: Doc = {
 function concat2(x: Doc, y: Doc): Doc {
   switch (x.type) {
     case "nil":
-      return doc2;
+      return y;
     case "text":
       return {
         type: "text",
@@ -83,6 +91,109 @@ export function nest(level: number, doc: Doc): Doc {
         level: doc.level + level,
         doc: doc
       };
+    case "union":
+      return {
+        type: "union",
+        x: nest(level, doc.x),
+        y: nest(level, doc.y)
+      };
+  }
+}
+
+function union(x: Doc, y: Doc): Doc {
+  // invariant: every first line of x must be longer than than first
+  // line of y
+  return {
+    type: "union",
+    x,
+    y
+  };
+}
+
+function flatten(doc: Doc): Doc {
+  switch (doc.type) {
+    case "nil":
+      return nil;
+    case "text":
+      return {
+        type: "text",
+        content: doc.content,
+        doc: flatten(doc.doc)
+      };
+    case "line":
+      return {
+        type: "text",
+        content: " ",
+        doc: doc.doc
+      };
+    case "union":
+      // All layouts in the set should flatten to the same document.
+      return flatten(doc.x);
+  }
+}
+
+export function group(doc: Doc): Doc {
+  switch (doc.type) {
+    case "nil":
+      return nil;
+    case "text":
+      return {
+        type: "text",
+        content: doc.content,
+        doc: group(doc.doc)
+      };
+    case "line":
+      return union(
+        {
+          type: "text",
+          content: " ",
+          doc: flatten(doc.doc)
+        },
+        doc
+      );
+    case "union":
+      return union(group(doc.x), group(doc.y));
+  }
+}
+
+function fits(doc: Doc, w: number): boolean {
+  if (w < 0) {
+    return false;
+  }
+  switch (doc.type) {
+    case "nil":
+      return true;
+    case "line":
+      return true;
+    case "text":
+      return fits(doc.doc, w - doc.content.length);
+    case "union":
+      throw new Error(`Unsupported`);
+  }
+}
+
+function better(x: Doc, y: Doc, w: number, k: number): Doc {
+  return fits(x, w - k) ? x : y;
+}
+
+function best(doc: Doc, w: number, k: number): Doc {
+  switch (doc.type) {
+    case "nil":
+      return nil;
+    case "text":
+      return {
+        type: "text",
+        content: doc.content,
+        doc: best(doc.doc, w, k + doc.content.length)
+      };
+    case "line":
+      return {
+        type: "line",
+        level: doc.level,
+        doc: best(doc.doc, w, k + doc.level * INDENT_SPACES)
+      };
+    case "union":
+      return better(doc.x, doc.y, w, k);
   }
 }
 
@@ -92,7 +203,7 @@ function repeatChar(ch: string, n: number): string {
     .join("");
 }
 
-export function layout(doc: Doc): string {
+function layout(doc: Doc): string {
   switch (doc.type) {
     case "nil":
       return "";
@@ -100,5 +211,11 @@ export function layout(doc: Doc): string {
       return doc.content + layout(doc.doc);
     case "line":
       return "\n" + repeatChar(" ", doc.level) + layout(doc.doc);
+    case "union":
+      throw new Error(`No unions for layout!`);
   }
+}
+
+export function pretty(doc: Doc, w: number): string {
+  return layout(best(doc, w, 0));
 }
