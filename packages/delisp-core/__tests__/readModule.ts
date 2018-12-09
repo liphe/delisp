43ecb6efd72bf5b64ed1367c4a7aeaa20c43ca53
Tijn Kersjes <tijn@divbyzero.nl>
2018-12-09 18:17:52 +0100

    Add readModule

---
+++ packages/delisp-core/__tests__/readModule.ts
@@ -0,0 +1,39 @@
+import { readModule } from "../src/index";
+
+describe("ReadModule", () => {
+  it("should read all syntaxes", () => {
+    expect(readModule(" (sum 1 2 3) (+ 4 5) (inc 6) ")).toMatchObject({
+      type: "module",
+      body: [
+        {
+          type: "function-call",
+          fn: {
+            type: "variable-reference",
+            variable: "sum"
+          },
+          args: [
+            { type: "number", value: 1 },
+            { type: "number", value: 2 },
+            { type: "number", value: 3 }
+          ]
+        },
+        {
+          type: "function-call",
+          fn: {
+            type: "variable-reference",
+            variable: "+"
+          },
+          args: [{ type: "number", value: 4 }, { type: "number", value: 5 }]
+        },
+        {
+          type: "function-call",
+          fn: {
+            type: "variable-reference",
+            variable: "inc"
+          },
+          args: [{ type: "number", value: 6 }]
+        }
+      ]
+    });
+  });
+});
