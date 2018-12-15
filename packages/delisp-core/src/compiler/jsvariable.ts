235f73c3d266a864b8a405227f36a584f29445ec
David Vázquez Púa <davazp@gmail.com>
2018-12-15 23:00:00 +0100

    Prefix every delisp variable with $ to avoid collisions for keywords

--- packages/delisp-core/src/compiler/jsvariable.ts
+++ packages/delisp-core/src/compiler/jsvariable.ts
@@ -1,4 +1,12 @@
 export function varnameToJS(x: string): string {
+  // This prefix is intended to avoid generating reserved Javascript
+  // keywords. For instance, the Delisp variable `const` will be
+  // translated to $const and not `const`.
+  const prefix = "$";
+
+  // Instead of escaping specific characters, we have a list of
+  // allowed characters explicitly! That should make easier for us to
+  // catch issues in the future.
   const escapes = new Map(
     Object.entries({
       "!": "bang",
@@ -19,22 +27,24 @@ export function varnameToJS(x: string): string {
       "=": "eq"
     })
   );
-  // Instead of escaping specific characters, we have a list of
-  // allowed characters explicitly! That should make easier for us to
-  // catch issues in the future.
-  return x
-    .split("")
-    .map(ch => {
-      if (/[a-zA-Z0-9_]/.test(ch)) {
-        return ch;
-      } else if (escapes.has(ch)) {
-        const replacement: string = escapes.get(ch)!;
-        return "$" + replacement + "$";
-      } else {
-        throw new Error(
-          `Error: the variable ${x} contained a non-allowed character ${ch}.`
-        );
-      }
-    })
-    .join("");
+
+  const escapeName = (name: string) => {
+    return name
+      .split("")
+      .map((ch: string) => {
+        if (/[a-zA-Z0-9_]/.test(ch)) {
+          return ch;
+        } else if (escapes.has(ch)) {
+          const replacement: string = escapes.get(ch)!;
+          return "$" + replacement + "$";
+        } else {
+          throw new Error(
+            `Error: the variable ${x} contained a non-allowed character ${ch}.`
+          );
+        }
+      })
+      .join("");
+  };
+
+  return `${prefix}${escapeName(x)}`;
 }
