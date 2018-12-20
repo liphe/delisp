0958b2fd8d827338777c9c185500cc4d4cf0001e
Tijn Kersjes <tijn@divbyzero.nl>
2018-12-20 14:40:51 +0200

    Move primitives to runtime

--- packages/delisp-runtime/src/index.ts
+++ packages/delisp-runtime/src/index.ts
@@ -1,4 +1,6 @@
-import { primitives } from "@delisp/core";
+import primitives from "./primitives";
+
+export { primitives };
 
 export default Object.entries(primitives).reduce(
   (runtime, [name, def]) => ({ ...runtime, [name]: def.value }),
