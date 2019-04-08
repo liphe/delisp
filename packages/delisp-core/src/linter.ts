4c384de684c8e4fe4de34e7d9dcd275eba19134e
Tijn Kersjes <tijn@divbyzero.nl>
2019-04-09 00:09:06 +0200

    Implement simple linter rules

--- packages/delisp-core/src/linter.ts
+++ packages/delisp-core/src/linter.ts
@@ -1,5 +1,59 @@
-import { Module } from "./syntax";
+import { Identifier, Module } from "./syntax";
+import { isModule, traverseModule } from "./syntax-utils";
+import { printHighlightedExpr } from "./error-report";
 
-export function lintModule(_m: Module): void {
-  // @TODO
+function noUnusedVars(m: Module): void {
+  const used: Set<Identifier> = new Set();
+
+  traverseModule(
+    m,
+    (current, scope) => {
+      if (isModule(current)) {
+        return;
+      } else if (current.node.tag === "variable-reference") {
+        const binding = scope[current.node.name];
+        if (binding) {
+          used.add(binding.identifier);
+        }
+      } else if (current.node.tag === "export") {
+        const binding = scope[current.node.value.name];
+        if (binding) {
+          used.add(binding.identifier);
+        }
+      }
+    },
+    (current, scope) => {
+      Object.entries(scope)
+        .filter(([_, binding]) => !used.has(binding.identifier))
+        .filter(([_, binding]) => binding.node === current)
+        .forEach(([name, binding]) => {
+          console.warn(
+            printHighlightedExpr(
+              `"${name}" is defined but never used (no-unused-vars)`,
+              binding.identifier.location
+            )
+          );
+        });
+    }
+  );
+}
+
+function noEmptyLet(m: Module): void {
+  traverseModule(m, curr => {
+    if (!isModule(curr) && curr.node.tag === "let-bindings") {
+      if (curr.node.bindings.length === 0) {
+        console.warn(
+          printHighlightedExpr(
+            `no variables bound in let expression (no-empty-let)`,
+            curr.location
+          )
+        );
+      }
+    }
+  });
+}
+
+export function lintModule(m: Module): void {
+  noUnusedVars(m);
+  noEmptyLet(m);
 }
