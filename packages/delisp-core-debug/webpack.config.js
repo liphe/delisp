fb1298dac2df4cdbce3d37abd521eeb365f6c1b7
David Vázquez Púa <davazp@gmail.com>
2019-05-15 20:12:50 +0200

    Integrate webpack

---
+++ packages/delisp-core-debug/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "eval-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
