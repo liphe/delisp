6216ef83ed06d327a3936d57c007d6d9fbebb3f2
David Vázquez Púa <davazp@gmail.com>
2019-06-12 19:20:26 +0200

    Implement primitives for tagged values

--- packages/delisp-runtime/src/index.ts
+++ packages/delisp-runtime/src/index.ts
@@ -6,3 +6,24 @@ export default Object.entries(primitives).reduce(
   (runtime, [name, def]) => ({ ...runtime, [name]: def.value }),
   {}
 );
+
+//
+// Variants
+//
+
+interface TaggedValue {
+  tag: string;
+  value: unknown;
+}
+
+export function matchTag(
+  obj: TaggedValue,
+  cases: { [label: string]: (value: unknown) => unknown }
+): unknown {
+  const handler = cases[obj.tag];
+  return handler(obj.value);
+}
+
+export function tag(tag: string, value: unknown): TaggedValue {
+  return { tag, value };
+}
