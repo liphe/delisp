2bb5738c5e8db8e4d36f1f88e209f101390c503d
David Vázquez Púa <davazp@gmail.com>
2019-07-07 11:15:18 +0200

    Fix tests

--- packages/delisp-core/__tests__/unify.ts
+++ packages/delisp-core/__tests__/unify.ts
@@ -1,58 +1,50 @@
 import { generateUniqueTVar } from "../src/type-generate";
-import {
-  app,
-  fn,
-  number,
-  record,
-  userDefined,
-  var,
-  vector
-} from "../src/types";
+import * as T from "../src/types";
 import { unify } from "../src/unify";
 
 describe("Unification", () => {
   it("should perform an occur check", () => {
-    const t1 = var("t1");
-    const t2 = vector(t1);
+    const t1 = T.var("t1");
+    const t2 = T.vector(t1);
     const result = unify(t1, t2, {});
     expect(result.tag).toBe("unify-occur-check-error");
   });
 
   describe("Application", () => {
     it("should catch function arity mismatches", () => {
-      const t1 = fn([number, number], var("_"), number);
-      const t2 = fn([number], var("_"), number);
+      const t1 = T.fn([T.number, T.number], T.var("_"), T.number);
+      const t2 = T.fn([T.number], T.var("_"), T.number);
       const result = unify(t1, t2, {});
       expect(result.tag).toBe("unify-mismatch-error");
     });
     it("should catch operator mismatches", () => {
-      const t1 = vector(number);
-      const t2 = fn([], var("_"), number);
+      const t1 = T.vector(T.number);
+      const t2 = T.fn([], T.var("_"), T.number);
       const result = unify(t1, t2, {});
       expect(result.tag).toBe("unify-mismatch-error");
     });
     it("should unify the operator", () => {
-      const t1 = var("r");
-      const t2 = var("r");
-      const result = unify(app(t1), app(t2), {});
+      const t1 = T.var("r");
+      const t2 = T.var("r");
+      const result = unify(T.app(t1), T.app(t2), {});
       expect(result.tag).toBe("unify-success");
     });
   });
 
   describe("Records", () => {
     it("with different head and same tail should not unify", () => {
-      const r = var("r");
-      const t1 = record([{ label: ":x", type: number }], r);
-      const t2 = record([{ label: ":y", type: number }], r);
+      const r = T.var("r");
+      const t1 = T.record([{ label: ":x", type: T.number }], r);
+      const t2 = T.record([{ label: ":y", type: T.number }], r);
       const result = unify(t1, t2, {});
       expect(result.tag).toBe("unify-mismatch-error");
     });
 
     it("with multiple different head and same tail should not unify", () => {
-      const r = var("r");
-      const t1 = record([{ label: ":x", type: number }], r);
-      const t2 = record(
-        [{ label: "z", type: number }, { label: ":y", type: number }],
+      const r = T.var("r");
+      const t1 = T.record([{ label: ":x", type: T.number }], r);
+      const t2 = T.record(
+        [{ label: "z", type: T.number }, { label: ":y", type: T.number }],
         r
       );
       const result = unify(t1, t2, {});
@@ -60,17 +52,20 @@ describe("Unification", () => {
     });
 
     it("closed rows with different labels dont unify", () => {
-      const t1 = record([{ label: ":x", type: number }]);
-      const t2 = record([{ label: ":z", type: number }]);
+      const t1 = T.record([{ label: ":x", type: T.number }]);
+      const t2 = T.record([{ label: ":z", type: T.number }]);
       const result = unify(t1, t2, {});
       expect(result.tag).toBe("unify-mismatch-error");
     });
 
     it("should unify records open multiple times", () => {
-      const r1 = record([{ label: ":x", type: number }], generateUniqueTVar());
-      const r2 = record([{ label: ":x", type: number }]);
-      const t1 = fn([r1], generateUniqueTVar(), r1);
-      const t2 = fn([r2], generateUniqueTVar(), r2);
+      const r1 = T.record(
+        [{ label: ":x", type: T.number }],
+        generateUniqueTVar()
+      );
+      const r2 = T.record([{ label: ":x", type: T.number }]);
+      const t1 = T.fn([r1], generateUniqueTVar(), r1);
+      const t2 = T.fn([r2], generateUniqueTVar(), r2);
       const result = unify(t1, t2, {});
       expect(result.tag).toBe("unify-success");
     });
@@ -78,16 +73,16 @@ describe("Unification", () => {
 
   describe("User defined types", () => {
     it("should unify with themselves", () => {
-      const t1 = userDefined("A");
-      const t2 = userDefined("A");
+      const t1 = T.userDefined("A");
+      const t2 = T.userDefined("A");
       const result = unify(t1, t2, {});
       expect(result.tag).toBe("unify-success");
     });
 
     it("should not unify with its definition", () => {
-      const t1 = number;
-      const t2 = userDefined("A");
-      const result = unify(t1, t2, { a: number });
+      const t1 = T.number;
+      const t2 = T.userDefined("A");
+      const result = unify(t1, t2, { a: T.number });
       expect(result.tag).toBe("unify-mismatch-error");
     });
   });
