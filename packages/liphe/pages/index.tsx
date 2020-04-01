27cb839ba82ac51a3ed824bee228c0f4b1433908
David Vázquez Púa <davazp@gmail.com>
2020-04-01 22:25:12 +0200

    Add pprinter example

--- packages/liphe/pages/index.tsx
+++ packages/liphe/pages/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Homepage() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>Liphe</h1>
+      <Link href="/pprinter">Pretty printer</Link>
+    </>
+  );
 }
---
+++ packages/liphe/pages/pprinter.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PPrinter() {
+  return <h1>Pretty printer</h1>;
+}
