998f82a6d0cbbe756bf65c115dfcc38bca28bae3
Tijn Kersjes <tijn@divbyzero.nl>
2020-04-08 20:15:46 +0200

    Fix BooleanExplorer

--- packages/liphe/components/Explorer/Boolean.tsx
+++ packages/liphe/components/Explorer/Boolean.tsx
@@ -4,5 +4,5 @@ import * as React from "react";
 export const BooleanExplorer: React.FC<{ value: Delisp.SBoolean }> = ({
   value,
 }) => {
-  return <span>{value.node.value}</span>;
+  return <span>{value.node.value ? "true" : "false"}</span>;
 };
